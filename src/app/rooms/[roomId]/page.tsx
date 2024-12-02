5c87f76fb6eae4c7fd16a6b5c217ebb14b39e93b
Jahnavi <ajahnavi2703@gmail.com>
2024-12-02 22:11:55 +0530

    your rooms page, middleware, editing a room

--- src/app/rooms/[roomId]/page.tsx
+++ src/app/rooms/[roomId]/page.tsx
@@ -5,11 +5,12 @@ import { Badge } from "@/components/ui/badge"
 import { TagsList } from "@/components/tags-list";
 import { CodeBuddyVideo } from "./video-player";
 import { splitTags } from "@/lib/utils";
+import { unstable_noStore } from "next/cache";
 
 
 export default async function RoomPage(props: {params: { roomId: string } }) {
+    unstable_noStore();
     const roomId = props.params.roomId;
-
     const room = await getRoom(roomId);
 
     if(!room) {
